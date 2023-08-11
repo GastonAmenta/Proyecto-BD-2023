a6bacae287f856732251cbe3f2dce1808b2804e1
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-08-11 16:50:59 -0300

    hago el login y hago conexion con sqlserver comentada

--- controllers/login.php
+++ controllers/login.php
@@ -1,5 +1,27 @@
 <?php
+require_once "../includes/config.php";
+
+if (!empty($_POST)) {
+    $dni = $_POST['dni'];
+    $clave = $_POST['clave'];
+}
 
 
+
+/*if (!empty($_POST)) {
+    $dni = $_POST['dni'];
+    $clave = $_POST['clave'];
+
+    $sqlquery = "SELECT * FROM Persona WHERE DNI = 123 AND Clave= 123";
+    $params = array(1, "some data");
+    $sqlresult = sqlsrv_query($conn, $sqlquery, $params);
+    if ($sqlquery === false) {
+        die('Error de consulta: ' . sqlsrv_errors($conn));
+    }
+    $while($row = sqlsrv_fetch_object($sqlresult, SQLSRV_FETCH_ASSOC)){
+        print_r($row)};
+}
+
+*/
 $view = "login";
 require_once "../views/layout.php";
