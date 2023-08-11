c41c448786381ef6175766bd1ae101f419109392
Tsifok <hermanmatias03@gmail.com>
2023-08-11 15:48:10 -0300

    termino el html css de registro

--- views/register.php
+++ views/register.php
@@ -1,14 +1,25 @@
-       
+<div class="topnav_register">
+    <a href="../index.php">
+        <img src="../img/banco_provincia.png" alt="banco_provincia.pnh">
+    </a>
+</div>
 
-<div class="form_container">
+<div class = "form_container">
         <form action="">
-            <div> <input type="text" id="name" placeholder="Nombre"> </div>
-            <div> <input type="text" id="cuil" placeholder="CUIL/CUIT"> </div>
-            <div> <input type="text" id="surname" placeholder="Apellido"> </div>
-            <div> <input type="email"id="email" placeholder="Email"> </div>            
-            <div> <input type="password" id="password" placeholder="Contraseña"> </div>
-            <div> <input type="password" id="conf_password" placeholder="Repetir contraseña"> </div>
+            <div class = "input_reg_form"> <input type="text" id="name" placeholder="Nombre"> </div>
+            <div class = "input_reg_form"> <input type="text" id="cuil" placeholder="CUIL/CUIT"> </div>
+            <div class = "input_reg_form"> <input type="text" id="surname" placeholder="Apellido"> </div>
+            <div class = "input_reg_form"> <input type="email"id="email" placeholder="Email"> </div>            
+            <div class = "input_reg_form"> <input type="password" id="password" placeholder="Contraseña"> </div>
+            <div class = "input_reg_form"> <input type="password" id="conf_password" placeholder="Repetir contraseña"> </div>
+            <div class = "btns_register" >
+                <button class = "btn_reg_form">Registrarme</button>
+                <div class = div_href_login>
+                    <a class= "href_login" href="">Iniciar sesion</a>
+                </div>
+            </div>            
         </form>
+        
 </div>
 
 
