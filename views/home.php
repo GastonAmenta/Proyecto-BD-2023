25fe01bc6fe24e10e671d560c0246fc0e605c61f
Lucio <luchito14567@gmail.com>
2023-08-11 16:53:52 -0300

    pagina principal

--- views/home.php
+++ views/home.php
@@ -1,50 +1,52 @@
-<div class="container">
-    <div class="container_button">
-        <div class="container_img">
-            <img src="../img/logo_2021_S.svg" alt="">
+<body style="background-color: #EBEBEB">
+    <div class="container">
+        <div class="container_button">
+            <div class="container_img">
+                <img src="../img/logo_2021_S.svg" alt="">
+            </div>
+            <div class="container_info">
+                <button>Personas</button>
+                <button>Empresas</button>
+                <button>Emprendedores</button>
+                <button>Provincias compras</button>
+                <button>Turnos web</button>
+                <a class="especial">BIP Personas</a>
+                <a class="especial">BIP Empresas</a>
+            </div>
         </div>
-        <div class="container_info">
-            <button>Personas</button>
-            <button>Empresas</button>
-            <button>Emprendedores</button>
-            <button>Provincias compras</button>
-            <button>Turnos web</button>
-            <a class="especial">BIP Personas</a>
-            <a class="especial">BIP Empresas</a>
-        </div>
-    </div>
 
 
-    <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="../img/banner_empleadas_domesticas_L.jpg" class="d-block w-100" alt="...">
+        <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="../img/banner_empleadas_domesticas_L.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="../img/cdni_arba_automotor_mejora_L.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="../img/banner_home_justina_L.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="../img/referidos_L.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="../img/cabezal_home_provincia_compras_ddn_L.jpg" class="d-block w-100" alt="...">
+                </div>
             </div>
-            <div class="carousel-item">
-            <img src="../img/cdni_arba_automotor_mejora_L.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="../img/banner_home_justina_L.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="../img/referidos_L.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="../img/cabezal_home_provincia_compras_ddn_L.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="container_login-padre">
+            <div class="container_login">
+                <div class="container_logo">
+                    <img src="../img/pngwing.com.png" alt="">
+                </div>
+                <div class="container_info2">
+                    <a href="../controllers/login.php">Iniciar Sesión</a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="container_login-padre">
-        <div class="container_login">
-            <div class="container_logo">
-                <img src="../img/pngwing.com.png" alt="">
-            </div>
-            <div class="container_info2">
-                <button>Iniciar Sesión</button>
-            </div>
-        </div>
-    </div>
-</div>
+</body>
 
 
 
