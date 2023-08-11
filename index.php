53a9060cbeca5934a4e17b8b2c30547726ea8f34
Lucio <luchito14567@gmail.com>
2023-08-11 14:12:36 -0300

    sigo pagina principal

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <div class='container'>
     <div class="container-header">
         <img src="img/logo_2021_S.svg" alt="">
+        <button>Provincias compas</button>
+        <button>Turnos web</button>
+        <button>BIP Personas</button>
+        <button>BIP Empresas</button>
     </div>
     <div id="carouselExampleSlidesOnly" class="carousel slide container-carrusel" data-bs-ride="carousel">
     <div class="carousel-inner">
