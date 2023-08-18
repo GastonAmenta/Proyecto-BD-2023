3b8f0f836f74eabac45062d2c9e49f35a9f5bbe9
Tsifok <hermanmatias03@gmail.com>
2023-08-18 15:22:40 -0300

    register

--- views/register.php
+++ views/register.php
@@ -1,4 +1,4 @@
-<div class= "topnav_register">
+    <div class= "topnav_register">
     <div class = "hola">
         <a href= "../index.php">
             <img src="../img/banco_provincia.png" alt="banco_provincia.pnh">
@@ -7,21 +7,33 @@
 </div>
 
 <div class = "form_container">
-        <form action="">
-            <div class = "input_reg_form"> <input type="text" id="name" placeholder="Nombre"> </div>
-            <div class = "input_reg_form"> <input type="text" id="cuil" placeholder="CUIL/CUIT"> </div>
-            <div class = "input_reg_form"> <input type="text" id="surname" placeholder="Apellido"> </div>
-            <div class = "input_reg_form"> <input type="email"id="email" placeholder="Email"> </div>            
-            <div class = "input_reg_form"> <input type="password" id="password" placeholder="Contraseña"> </div>
-            <div class = "input_reg_form"> <input type="password" id="conf_password" placeholder="Repetir contraseña"> </div>
+        <form action="" method="POST">
+            <div class = "input_reg_form"> <input type="number" name="dni" placeholder="DNI" required oninput="this.value = this.value.replace(/[^0-9]/g, '');"> </div>                
+            <div class = "input_reg_form"> <input type="number" name="cuil" placeholder="CUIL/CUIT" required> </div>
+            <div class = "input_reg_form"> <input type="text" name="name" placeholder="Nombre" required> </div>
+            <div class = "input_reg_form"> <input type="text" name="surname" placeholder="Apellido" required> </div>
+            <div class = "input_reg_form"> <input type="email"name="email" placeholder="Email" required> </div>            
+            <div class = "input_reg_form"> <input type="password" name="password" placeholder="Contraseña" required> </div>
+            <div class = "input_reg_form"> <input type="password" name="conf_password" placeholder="Repetir contraseña" required> </div>
+            <div class = "input_reg_form"> <input type="number" name="cell_nmb" placeholder="Telefono" required> </div>
+            <div class = "input_reg_form"> <input type="text" name="addres" placeholder="Direccion" required> </div>
+            <div class = "input_reg_form"> <input type="date" name="birth_date" placeholder="Fecha de nacimiento" required> </div>
+
+            <div class = "input_reg_form"> <select>
+                    <option value="lol"></option>
+                </select> 
+            </div>
+
+
+
+
             <div class = "btns_register" >
                 <button class = "btn_reg_form">Registrarme</button>
                 <div class = div_href_login>
-                    <a class= "href_login" href="">Iniciar sesion</a>
+                    <a class= "href_login" href="login.php">Iniciar sesion</a>
                 </div>
             </div>            
-        </form>
-        
+        </form>        
 </div>
 
 
