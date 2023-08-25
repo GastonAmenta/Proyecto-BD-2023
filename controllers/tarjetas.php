3670fe4402afef1781c66dd514a50a78d720090e
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-08-25 17:02:48 -0300

    cambios de home y login, hago pagina de tarjetas

---
+++ controllers/tarjetas.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../includes/config.php";
+
+if (!$_SESSION['user']) {
+    header("Location: login.php");
+}
+$id = $_SESSION["user"]['ID'];
+$sqlTarjeta = "SELECT * FROM caja_ahorro WHERE usuarios_id = '" . $id . "' AND fecha_baja IS NULL";
+$resultTarjeta = mysqli_query($conn, $sqlTarjeta);
+if(!$resultTarjeta){
+    die("Error de consulta: " .mysqli_error($conn));
+}
+$rowTarjeta = mysqli_fetch_array($resultTarjeta, MYSQLI_ASSOC);
+
+print_r($rowTarjeta);   
+$view = "tarjetas";
+require_once "../views/layout.php";
