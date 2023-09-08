d6a9e41c0a46c8df4be71925839fd4552621d452
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-09-08 16:43:31 -0300

    realizo pagina transferencias

--- controllers/transfer.php
+++ controllers/transfer.php
@@ -1,5 +1,56 @@
-<?php 
+<?php
 require_once "../includes/config.php";
+if (!$_SESSION['user']) {
+    header("Location: login.php");
+}
 
+$id = $_SESSION["user"]['ID'];
+if (!empty($_POST)) {
+    $alias = $_POST['alias'];
+    $monto = $_POST['monto'];
+    $descripcion = $_POST['descripcion'];
+    $clave = $_POST['clave'];
+
+    //checks
+    $sqlCheckMount = "SELECT monto_disp FROM caja_ahorro WHERE usuarios_id ='" . $id . "'";
+    $resultCheckMount = mysqli_query($conn, $sqlCheckMount);
+    if (!$resultCheckMount) {
+        die("Error de consulta: " . mysqli_error($conn));
+    }
+    $rowMonto = mysqli_fetch_assoc($resultCheckMount);
+
+    $sqlUserTransfer = "SELECT usuarios_id FROM caja_ahorro WHERE alias = '" . $alias . "'";
+    $resultUserTransfer = mysqli_query($conn, $sqlUserTransfer);
+    if (!$resultUserTransfer) {
+        die("Error de consulta: " . mysqli_error($conn));
+    }
+    $transferId = mysqli_fetch_assoc($resultUserTransfer);
+    //print_r($transferId['usuarios_id']);
+
+    if (($rowMonto >= $monto) && ($_SESSION['user']['clave'] === $clave) ) {
+
+        $sqlTransferir1 = "UPDATE caja_ahorro SET monto_disp = monto_disp -'" . $monto . "' WHERE usuarios_id ='" . $id . "'";
+        $resultTransferir1 = mysqli_query($conn, $sqlTransferir1);
+        if (!$resultTransferir1) {
+            die("Error de consulta: " . mysqli_error($conn));
+        }
+
+        $sqlTransferir2 = "UPDATE caja_ahorro SET monto_disp = monto_disp +'" . $monto . "' WHERE alias ='" . $alias . "'";
+        $resultTransferir2 = mysqli_query($conn, $sqlTransferir2);
+        if (!$resultTransferir2) {
+            die("Error de consulta: " . mysqli_error($conn));
+        }
+
+        if ($resultTransferir1 && $resultTransferir2) {
+            $sqlTransferir3 = "INSERT INTO transacciones (fecha_hora, cuenta_origen, cuenta_destino, tipo_transaccion, monto, descripcion, estado) VALUES (now(), '" . $id . "' , '" . $transferId['usuarios_id'] . "', 'transferencia' , '" . $monto . "', '" . $descripcion . "' , 'exitosa')";
+            $resultTransferir3 = mysqli_query($conn, $sqlTransferir3);
+
+            if (!$resultTransferir3) {
+                die("Error de consulta: " . mysqli_error($conn));
+            }
+
+        }
+    }
+}
 $view = "transfer";
 require_once "../views/layout.php";
--- views/transfer.php
+++ views/transfer.php
@@ -1,1 +1,52 @@
-hola
+<div class="container">
+    <div class="container_button">
+        <div class="container_img">
+            <a href="home.php">
+                <img src="../img/logo_2021_S.svg" alt="">
+            </a>
+        </div>
+        <div class="container_info">
+            <button>Personas</button>
+            <button>Empresas</button>
+            <button>Emprendedores</button>
+            <button>Provincias compras</button>
+            <button>Turnos web</button>
+            <a class="especial">BIP Personas</a>
+            <a class="especial">BIP Empresas</a>
+        </div>
+    </div>
+    <div class="back_button">
+        <a href="cards.php">
+            <i class="fa fa-arrow-left" style="font-size:25px;color:black;font-style:inherit;"> Atras</i>
+        </a>
+        <div class="my_cards">
+            <span>Transferir</span>
+        </div>
+    </div>
+    <div class="form_container">
+        <form action="../controllers/transfer.php" method="POST" id="form_register">
+            <div class="texto">
+                <span>Ingrese el alias de la cuenta destino</span>
+            </div>
+            <div class="input_reg_form"> <input type="text" name="alias" placeholder="Alias" required> </div>
+
+            <div class="texto">
+                <span>Ingrese el monto</span>
+            </div>
+            <div class="input_reg_form"> <input type="number" name="monto" placeholder="Monto" required> </div>
+
+            <div class="texto">
+                <span>Ingrese la descripcion</span>
+            </div>
+            <div class="input_reg_form"> <input type="text" name="descripcion" placeholder="Descripcion" required> </div>
+
+            <div class="texto">
+                <span>Ingrese su contraseña</span>
+            </div>
+            <div class="input_reg_form"> <input type="password" name="clave" placeholder="Contraseña" required> </div>
+
+
+            <div class="input_reg_form"> <input class="btn_reg_form" type="submit"> </div>
+        </form>
+    </div>
+</div>
